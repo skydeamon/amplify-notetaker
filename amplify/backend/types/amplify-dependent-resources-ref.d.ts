9cbff637d3046002068afc069cec257bfa29eedb
skydeamon <skycharmingdeamon@gmail.com>
2022-05-12 06:38:30 +0200

    Basic User interface and upload

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifynotetaker38701ba5": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "amplifynotetaker": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { withAuthenticator } from 'aws-amplify-react';
+import React, { Component }  from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    notes: [
+      {
+        id: 1,
+        note: "Hello World"
+      }
+    ]
+  }
+
+  render() {
+    const { notes } = this.state;
+    return (
+      <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
+        <h1 className="code f2-1">
+          Amplify Notetaker
+        </h1>
+        {/* Note Form */}
+        <form className="mb3">
+         <input types="text" className="pa2 f4" placeholder="write your note" />
+         <button className="pa2 f4">
+            Add Note
+         </button>
+        </form>
+
+        {/* Note List */}
+        <div>
+          {notes.map(item =>(
+            <div key={item.id} className="flex items-center">
+              <li className="list pa1 f3">
+                {item.note}
+              </li>
+              <button className="bg-transparent bn f4">
+                <span>&times;</span>
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+// export default withAuthenticator(App, {includeGreetings: true});
